95a6d2b85b6bb9e7605b5faefccac93108d6d0ed
jmjin2 <99165006+jmjin2@users.noreply.github.com>
2023-10-04 22:19:08 +0900

    Update 4xDownsizing.sh
    frame 단위로 downsizing

--- 4xDownsizing.sh
+++ 4xDownsizing.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-input_root_dir="output_video"
-output_dir="4xdownsizing_video"
+input_root_dir="input_video"
+output_dir="output_video"
 
-for input_file in "$input_root_dir"/*.mp4; do
+for input_file in "$input_root_dir"/*.jpg; do
     base=$(basename "$input_file")
     filename="${base%.*}"
     
-    output_file="$output_dir/$filename"_4xDownsized.mp4
+    output_file="$output_dir/$filename".jpg
     
     ffmpeg -i "$input_file" -vf "scale=iw/4:ih/4" -c:a copy "$output_file"
 done
